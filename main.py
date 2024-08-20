8bc04d149a5b2ab196eb44f707acfa910c96ddac
Dimitri Croes <dimi.croes@gmail.com>
2024-08-20 21:04:16 +0200

    make main page mockup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 server = Flask(__name__)
 
 @server.route('/',methods=['GET'])
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 server.run('0.0.0.0',5000,debug=True)
